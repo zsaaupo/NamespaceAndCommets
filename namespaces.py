e47ccdf3ffc8390ed145f60f10c808f5c15e3906
Md. Zakaria Sarker <46224041+zsaaupo@users.noreply.github.com>
2021-05-01 16:59:49 +0600

    Add files via upload

---
+++ namespaces.py
@@ -0,0 +1,14 @@
+"""
+Namespaces
+"""
+
+a = 7
+b = 8
+c = 7
+d = 8
+# a and c variable's value are same so there id (namespaces) is also gonna be same
+print(id(a))
+print(id(c))
+# b and d variable's value are same so there id (namespaces) is also gonna be same
+print(id(b))
+print(id(d))
---
+++ statement_and_comment.py
@@ -0,0 +1,14 @@
+"""
+Every programeble line is a statement
+    I'm giving some example in here.
+"""
+print(__doc__) # Executing Dunder Doc (in line Comment)
+# First statement (comment)
+a = "Hello"
+# Second statement (Comment)
+b = "Python"
+print(a+b) # Execution satement (in line Comment)
+
+"""
+End
+"""
